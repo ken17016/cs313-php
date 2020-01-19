094cabf74d212a3ad95dbff107f087e0d69b04b5
AJ <ajkenner1@gmail.com>
2020-01-18 23:36:59 -0700

    Fixed image pointing.

--- web/assignments/Week02/02Prove.php
+++ web/assignments/Week02/02Prove.php
@@ -16,9 +16,9 @@
     <blockquote style="font-style:italic; font-family:'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif">Some believe it is only great power that can hold evil in check,
       but that is not what I have found. It is the small everyday deeds of ordinary
       folk that keep the darkness at bay.Small acts of kindness and love.</blockquote>
-       <img src="../../Images/porg1.jpg" alt="A picture of a porg." style="width:400px; height:400px; display:block; margin:0 auto;"/>
-       <img src="../../Images/JaceBeleren.jpg" alt="A picture of a Jace Beleren." style="width:400px; height:400px; display:block; margin:0 auto;"/>
-       <img src="../../Images/nightSky.jpg" alt="A picture of a night sky." style="width:400px; height:400px; display:block; margin:0 auto;"/>
+       <img src="../../Images/porg1.jpg" alt="A picture of a porg." style="height:400px; display:block; margin:0 auto;"/>
+       <img src="../../Images/JaceBeleren.jpg" alt="A picture of a Jace Beleren." style="height:400px; display:block; margin:0 auto;"/>
+       <img src="../../Images/nightSky.jpg" alt="A picture of a night sky." style="height:400px; display:block; margin:0 auto;"/>
        
   </body>
 </html>

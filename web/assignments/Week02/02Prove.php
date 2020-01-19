3e458c5dd38643dc32d5578b41eef6ae8575781b
AJ <ajkenner1@gmail.com>
2020-01-18 23:47:55 -0700

    Fixed image pointing.

--- web/assignments/Week02/02Prove.php
+++ web/assignments/Week02/02Prove.php
@@ -3,22 +3,23 @@
   <head>
     <meta charset="utf-8" />
     <title>AJ's Home Page</title>
+    <link rel="stylesheet" type="text/css" href="./02Prove.css">
   </head>
   <body>
     <header>
       <h1>Welcome to AJ Kenner's Home Page</h1>
-      <span><a href="index.php">CS 313 Assignments</a></span>
+      <span><a href="../../index.php">CS 313 Assignments</a></span>
       <hr />
     </header>
-    <p style="font-size:20px; font-family:Arial">
-      Hello, my name is AJ. I like Pokemon, Magic the Gathering, the night sky and Starwars. 
+    <p>
+      Hello, my name is AJ. I like Pokemon, Lord of the Rings, The Hobbit, Magic the Gathering, the night sky and Starwars. 
     </p>
-    <blockquote style="font-style:italic; font-family:'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif">Some believe it is only great power that can hold evil in check,
+    <blockquote>Some believe it is only great power that can hold evil in check,
       but that is not what I have found. It is the small everyday deeds of ordinary
       folk that keep the darkness at bay.Small acts of kindness and love.</blockquote>
-       <img src="../../Images/porg1.jpg" alt="A picture of a porg." style="height:400px; display:block; margin:0 auto;"/>
-       <img src="../../Images/JaceBeleren.jpg" alt="A picture of a Jace Beleren." style="height:400px; display:block; margin:0 auto;"/>
-       <img src="../../Images/nightSky.jpg" alt="A picture of a night sky." style="height:400px; display:block; margin:0 auto;"/>
+       <img src="../../Images/porg1.jpg" alt="A picture of a porg." />
+       <img src="../../Images/JaceBeleren.jpg" alt="A picture of a Jace Beleren." >
+       <img src="../../Images/nightSky.jpg" alt="A picture of a night sky." >
        
   </body>
 </html>

a85e238c7c61fbf6a8357bbf6515eabab62e26d6
AJ <ajkenner1@gmail.com>
2020-01-18 22:29:15 -0700

    Added in index page.

--- web/index.php
+++ web/index.php
@@ -1,5 +1,61 @@
-<?php
+DOCTYPE html>
+	<html lang="en-us">
+	<head>
+	    <meta charset="utf-8" />
+	        <title>AJ Kenner's assignments</title>
+		    <link rel="stylesheet" type="text/css" href="./StyleSheets/indexStyleSheet.css">
+		    </head>
+<body>
+    <h1>CS 313 assignments </h1>
+    <hr />
+<hr>
+<?php echo "<div = class='info'>Today is " . date("l") . "<br/>";
+echo "The time is " . date("h:i:sa") . "</div>";?>
+<hr>
+    <ul >
+        <li>
+            <a href="./hello.html">assignment 01</a>
+        </li>
+        <li>
+            <a href="./assignments/Week02/02Prove.html">assignment 02</a>
+        </li>
+        <li>
+            <a href="./assignment03.html">assignment 03</a>
+        </li>
+        <li>
+            <a href="./assignment04.html">assignment 04</a>
+        </li>
+        <li>
+            <a href="./assignment05.html">assignment 05</a>
+        </li>
+        <li>
+            <a href="./assignment06.html">assignment 06</a>
+        </li>
+        <li>
+            <a href="./assignment07.html">assignment 07</a>
+        </li>
+        <li>
+            <a href="./assignment08/PlanesWalkerAcademy.html">assignment 08</a>
+        </li>
+        <li>
+            <a href="./assignment09.html">assignment 09</a>
+        </li>
+        <li>
+            <a href="./assignment10.html">assignment 10</a>
+        </li>
+        <li>
+            <a href="./assignment11.html">assignment 11</a>
+        </li>
+        <li>
+            <a href="./assignment12.html">assignment 12</a>
+        </li>
+        <li>
+            <a href="./assignment13.html">assignment 13</a>
+        </li>
+        <li>
+            <a href="./assignment14.html">assignment 14</a>
+        </li>
+    </ul>
+</body>
+</html>
 
-  phpinfo();
-
-?>

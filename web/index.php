4d34161775c71d36a4fa9c344cd2d961e6d58cfd
AJ <ken17016@byui.edu>
2020-02-08 17:34:34 -0700

    Week05

--- web/index.php
+++ web/index.php
@@ -31,6 +31,9 @@ echo "<div>Local time is ". $localTime->format('m/d/Y, H:i:s') . "</div> ";?>
             <a href="./assignments/Week05/05Teach.php">Week 05</a>
         </li>
         <li>
+            <a href="./assignments/Week05/home.php">Week 05 Prove</a>
+        </li>
+        <li>
             <a href="./spoof.html">Week 06</a>
         </li>
         <li>

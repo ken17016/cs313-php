df2b8888a76e5eb901f2b9952d9a4b9d39a74e16
AJ <ken17016@byui.edu>
2020-02-17 18:40:29 -0700

    test06

--- web/assignments/Week06/login.php
+++ web/assignments/Week06/login.php
@@ -1,6 +1,21 @@
 <?php
-   session_start();
+  require('dbcon.php');
+  $db = get_db();
+    
+
+  $userName = htmlspecialchars($_POST["userName"]);
+  $password = &_POST["password"];
+   
+  $query = 'SELECT user_id usersname, userspassword FROM users WHERE usersname = :username';
+
+  $statement = $db->prepare($query);
+  $statement->bindvalue(":username", $userName, PDO::PARAM_STR); 
+  $statement->execute();
+
+  $user = $statement->fetchALL(PDO::FETCH_ASSOC);
+
+  if(password_verify($passowrd, $user[0]['userspassword'])){
+   $_SESSION['user_id']= $user[0]['users_id'];
+   $_SESSION['userName']= $user[0]['usersname'];
+  }
 ?>
-<html>
-<form action="log.php" method="post">
-</html>
